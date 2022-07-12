7ca07ed6b0a4f969b360be9f1c660babc0294a51
Gabriel Martins <no@email.com>
2022-07-11 21:03:14 -0300

    learning strings

--- boot.asm
+++ boot.asm
@@ -1,18 +1,37 @@
-mov ah, 0x0e
-mov al, 65 
-int 0x10
+[org 0x7c00]	;BIOS loads MBR into memory address 0x7c00 so every thing must be offset by this number.
+							; if we didnt do that, we would need something like this two lines below "mov al, [myString + 0x7c00]"
 
-loop:
-	cmp al, 'Z'
-	jge exit
-	add al, 33
-	int 0x10
-	add al, -31
-	int 0x10
-	jmp loop
+; call and ret 
+
+mov ax, 0
+int 0x16 ; wait for the input of a character
+
+mov ah, 0x0e
+int 0x10 ; print the given character
+
+mov ah, 0x0e
+mov bx, myString  
+
+printString: ;print our string (learning)
+	mov al, [bx] ; [] is a pointer, it is the points to the start of the string, same as C char *myString = "Hello World"
+	cmp al, 0 ; if we reach the end of the string (null terminated) stop.
+	je exit
+	int 0x10 ; print current character in al
+	inc bx ; increment the string pointer to get the next character
+	jmp printString
 
 exit:
-	jmp $
+	int 0x10
 
+myString:
+	db "Hello World", 0
+; the boot sector is 512 bytes long ending in 0x55aa, so we fill it with padding zeros 
+; $ means current code, $$ means beginning of current section, so jmp $ is an infinite loop, then all zeros until the 510th byte, then 0x55aa
+; memory layout is always inverted so 0xaa55 = 0x55, 0xaa
+
+jmp $ 
 times 510-($-$$) db 0
-db 0x55, 0xaa
+dw 0xaa55
+
+
+
